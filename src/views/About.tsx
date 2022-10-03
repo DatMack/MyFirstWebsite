435e32ea22c2e6310b1e707c810c90091ec15f96
John Schloss <j.schloss09@icloud.com>
2022-10-03 02:33:06 -0400

    editted the order of jobs

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -62,6 +62,28 @@ const AboutView = () => {
 
       <Section title='Work Experience' visible={false}>
         <div className='Section__item'>
+          <div className='Section__item-header'>PacMoore Process Technoligies</div>
+          <div className='Section__item-content'>
+            <div className='Section__item-subheader'>Forklift Driver 2021 to 2022</div>
+            <div className='Section__item-description'>
+              Tasked with making sure plant production team had materails on time, and move product out the system in a
+              timely manner so they could continue to produce prodcut. Also in charge of cleaning up the production area
+              and keeping it up to industry standards for food production.
+            </div>
+          </div>
+        </div>
+
+        <div className='Section__item'>
+          <div className='Section__item-content'>
+            <div className='Section__item-subheader'>Warehouse Management 2022 to Present</div>
+            <div className='Section__item-description'>
+              In charge of prodcut stored in the Warehouse, making sure that what was produced was stored in the
+              warehouse properly and ready to be shipped to the customer in a timely fashion. Also made sure that
+              product was seperated by, also in charge of disposing of, or destoying bad product.
+            </div>
+          </div>
+        </div>
+        <div className='Section__item'>
           <div className='Section__item-header'>United States Army</div>
           <div className='Section__item-content'>
             <div className='Section__item-subheader'>Infantry Radio Telecommunications Operator - 2011 to 2012</div>
@@ -81,29 +103,6 @@ const AboutView = () => {
               as soldiers but as people too. I have been to many Army leadership courses that have helped me learn how
               to be a better leader and that training has transfered over to the work enviorment and helped me become a
               strong worker and leader in the civilian workforce as well.
-            </div>
-          </div>
-        </div>
-
-        <div className='Section__item'>
-          <div className='Section__item-header'>PacMoore Process Technoligies</div>
-          <div className='Section__item-content'>
-            <div className='Section__item-subheader'>Forklift Driver 2021 to 2022</div>
-            <div className='Section__item-description'>
-              Tasked with making sure plant production team had materails on time, and move product out the system in a
-              timely manner so they could continue to produce prodcut. Also in charge of cleaning up the production area
-              and keeping it up to industry standards for food production.
-            </div>
-          </div>
-        </div>
-
-        <div className='Section__item'>
-          <div className='Section__item-content'>
-            <div className='Section__item-subheader'>Warehouse Management 2022 to Present</div>
-            <div className='Section__item-description'>
-              In charge of prodcut stored in the Warehouse, making sure that what was produced was stored in the
-              warehouse properly and ready to be shipped to the customer in a timely fashion. Also made sure that
-              product was seperated by, also in charge of disposing of, or destoying bad product.
             </div>
           </div>
         </div>
